58892beed296b8b134765cb110f8259b70426c0f
RytisMitkus <rytis@mitkus.lt>
2021-05-15 23:17:30 +0300

    first commit

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      hello: "",
+    };
+  },
+  methods: {
+    async fetchData() {
+      const { data } = await axios.get("/api");
+      this.hello = data.hello;
+      console.log(data);
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
+</script>
